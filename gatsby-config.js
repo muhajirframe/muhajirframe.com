8647ba1b50fc15d2e56a5534650f6a227ea59e1a
Muhammad Muhajir <muhajirframe@gmail.com>
2018-07-20 16:17:48 +0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+const config = require('./config/website')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
+module.exports = {
+  /* General Information */
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
+  },
+  /* Plugins */
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images/`,
+        name: 'images',
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitleAlt,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'fullscreen',
+        icons: [
+          {
+            src: '/favicon-196x196.png',
+            sizes: '196x196',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+    /* Must be placed at the end */
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+  ],
+}
